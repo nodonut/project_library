7b585bf9a5b1bc6975f42d40ac5b7b7905fec935
Apoorv Sohal <apoorvsohal@protonmail.com>
2020-02-23 17:52:04 +0530

    added read checkbox feature

--- script.js
+++ script.js
@@ -1,52 +1,29 @@
-const checkReadStatus = document.querySelector('input[type = "checkbox"]');
-
-checkReadStatus.addEventListener('click', e => {
-  e.target.setAttribute('class', 'checked');
-});
+// const checkReadStatus = document.querySelector('input[type = "checkbox"]');
 
 const submitButton = document.querySelector('button[type="submit"]');
 const bookTitle = document.querySelector('input[id="book_title"]');
 const bookAuthor = document.querySelector('input[id="book_author"]');
 const bookRead = document.querySelector('input[id="book_read"]');
 const bookBody = document.querySelector('.book-body');
-submitButton.addEventListener('click', () => {
-  // const book_title = bookTitle.value;
-  // bookBody.innerHTML +=
-  if (bookRead.classList.contains('checked')) {
-    const book = new Book(bookTitle.value, bookAuthor.value, bookRead.value);
-    addBookToLibrary(book);
 
-    myLibrary.forEach(item => {
-      let book_title = item.title;
-      let book_author = item.author;
-      bookBody.innerHTML += `<tr>
-    <td>${book_title}</td>
-    <td>${book_author}</td>
-    <td>
-      <form action="#">
-        <label> <input type="checkbox" checked="checked"/><span></span></label>
-      </form>
-    </td>
-  </tr>`;
-    });
+bookReadCounter = 0;
+
+bookRead.addEventListener('click', e => {
+  if (e.target.classList.contains('checked')) {
+    e.target.classList = '';
+    bookReadCounter = 0;
   } else {
-    const book = new Book(bookTitle.value, bookAuthor.value, bookRead.value);
-    addBookToLibrary(book);
-
-    myLibrary.forEach(item => {
-      let book_title = item.title;
-      let book_author = item.author;
-      bookBody.innerHTML += `<tr>
-    <td>${book_title}</td>
-    <td>${book_author}</td>
-    <td>
-      <form action="#">
-        <label> <input type="checkbox" /><span></span></label>
-      </form>
-    </td>
-  </tr>`;
-    });
+    e.target.classList = 'checked';
+    bookReadCounter = 1;
   }
+});
+
+submitButton.addEventListener('click', () => {
+  const book = new Book(bookTitle.value, bookAuthor.value, bookReadCounter);
+  addBookToLibrary(book);
+  clearTable();
+  render();
+  console.log(book);
 });
 
 let myLibrary = [];
@@ -59,4 +36,38 @@ function Book(title, author, read) {
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
+}
+
+function render() {
+  myLibrary.forEach(item => {
+    let book_title = item.title;
+    let book_author = item.author;
+    let book_read = item.read;
+
+    if (book_read == 1) {
+      bookBody.innerHTML += `<tr>
+      <td>${book_title}</td>
+      <td>${book_author}</td>
+      <td>
+      <form action="#">
+        <label> <input type="checkbox" checked="checked"/><span></span></label>
+      </form>
+    </td>
+  </tr>`;
+    } else {
+      bookBody.innerHTML += `<tr>
+      <td>${book_title}</td>
+      <td>${book_author}</td>
+      <td>
+      <form action="#">
+        <label> <input type="checkbox"/><span></span></label>
+      </form>
+    </td>
+  </tr>`;
+    }
+  });
+}
+
+function clearTable() {
+  bookBody.innerHTML = ``;
 }
